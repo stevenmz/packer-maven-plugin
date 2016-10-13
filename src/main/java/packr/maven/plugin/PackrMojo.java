52014cc5fff6139042c139ededc09123e15e9f3e
Steven Magaña-Zook <maganazook1@llnl.gov>
2016-10-13 10:27:30 -0700

    Reverting change to field names and addition of setters. Was not the issue with jenkins.

--- src/main/java/packr/maven/plugin/PackrMojo.java
+++ src/main/java/packr/maven/plugin/PackrMojo.java
@@ -45,20 +45,20 @@ public class PackrMojo extends AbstractMojo {
      * The bundle identifier of your Java application, e.g. "com.my.app". This
      * is for OS X targets (see {@link Platform#MacOS} ).
      */
-    @Parameter(property = "packr.bundleIdentifier")
-    private String _bundleIdentifier;
+    @Parameter(property = "bundleIdentifier")
+    private String bundleIdentifier;
 
     /**
      * list of file locations of the JAR files to package
      */
     @Parameter(property = "classpath")
-    private List<String> _classpath;
+    private List<String> classpath;
 
     /**
      * Name of the native executable, without extension such as ".exe"
      */
     @Parameter(property = "executable")
-    private String _executable;
+    private String executable;
 
     /**
      * Location of an AppBundle icon resource (.icns file) Note: This is for OS
@@ -66,13 +66,13 @@ public class PackrMojo extends AbstractMojo {
      *
      * @see Platform
      */
-    @Parameter(property = "packr.iconResource")
-    private File _iconResource;
+    @Parameter(property = "iconResource")
+    private File iconResource;
     /**
      * ZIP file location to an Oracle JDK build containing a JRE.
      */
     @Parameter(property = "jdk")
-    private String _jdk;
+    private String jdk;
     /**
      * You can put all the command line arguments into a JSON file. The JSON, if
      * provided, takes precedence over the other configuration values.
@@ -80,15 +80,15 @@ public class PackrMojo extends AbstractMojo {
      * Typically, you either specify JSON or you specify the configuration
      * parameters manually.
      */
-    @Parameter(property = "packr.jsonConfig")
-    private File _jsonConfig;
+    @Parameter(property = "jsonConfig")
+    private File jsonConfig;
 
     /**
      * The fully qualified name of the main class, using dots to delimit package
      * names
      */
     @Parameter(property = "mainClass")
-    private String _mainClass;
+    private String mainClass;
 
     /**
      * Minimize the JRE by removing directories and files as specified by an
@@ -96,54 +96,54 @@ public class PackrMojo extends AbstractMojo {
      * of the box.
      */
     @Parameter(property = "minimizeJre")
-    private String _minimizeJre;
+    private String minimizeJre;
 
     /**
      * The output directory.
      */
     @Parameter(property = "outDir")
-    private File _outDir;
+    private File outDir;
 
     /**
      * Defines the platform target for which to build an installer package. One
      * of "Windows32", "Windows64", "Linux32", "Linux64", "MacOS"
      */
     @Parameter(property = "platform")
-    private PackrConfig.Platform _platform;
+    private PackrConfig.Platform platform;
 
     /**
      * list of files and directories to be packaged next to the native
      * executable.
      */
     @Parameter(property = "resources")
-    private List<File> _resources;
+    private List<File> resources;
 
     /**
      * Prints more status information during processing, which can be useful for
      * debugging.
      */
-    @Parameter(property = "packr.verbose", defaultValue = "true")
-    private boolean _verbose;
+    @Parameter(property = "verbose", defaultValue = "true")
+    private boolean verbose;
     /**
      * List of arguments for the JVM, without leading dashes, e.g. "Xmx1G"
      */
     @Parameter(property = "vmArgs")
-    private List<String> _vmArgs;
+    private List<String> vmArgs;
 
     @Override
     public void execute() throws MojoExecutionException {
         getLog().info("Starting packr-maven-plugin...");
 
         // toString outputs the fields as a json object. Can be useful for debugging.
-        if (this._verbose) {
+        if (this.verbose) {
             getLog().info(this.toString());
         }
 
         PackrConfig config = null;
-        if (this._jsonConfig != null && this._jsonConfig.exists()) {
+        if (this.jsonConfig != null && this.jsonConfig.exists()) {
             //get values from JSON
             getLog().info("Using values from JSON configuration file.");
-            PackrJSONCommandLine commandline = new PackrJSONCommandLine(this._jsonConfig);
+            PackrJSONCommandLine commandline = new PackrJSONCommandLine(this.jsonConfig);
             try {
                 config = new PackrConfig(commandline);
             } catch (IOException ex) {
@@ -152,14 +152,14 @@ public class PackrMojo extends AbstractMojo {
             }
         } else {
             config = new PackrConfig();
-            config.platform = this._platform;
-            config.jdk = this._jdk;
-            config.executable = this._executable;
-            config.classpath = this._classpath;
-            config.mainClass = this._mainClass;
-            config.vmArgs = this._vmArgs;
-            config.minimizeJre = this._minimizeJre;
-            config.outDir = this._outDir;
+            config.platform = this.platform;
+            config.jdk = this.jdk;
+            config.executable = this.executable;
+            config.classpath = this.classpath;
+            config.mainClass = this.mainClass;
+            config.vmArgs = this.vmArgs;
+            config.minimizeJre = this.minimizeJre;
+            config.outDir = this.outDir;
         }
 
         // Try to catch some conditions that will make Packr throw exceptions
@@ -172,7 +172,7 @@ public class PackrMojo extends AbstractMojo {
 
         // See if the user specified any non-existent locations to add to the classpath
         List<String> validClasspathEntries = new ArrayList<>();
-        for (String cpEntry : _classpath) {
+        for (String cpEntry : classpath) {
             File fleEntry = new File(cpEntry);
             if (!fleEntry.exists()) {
                 getLog().warn(cpEntry + " is not a valid location. Entry will be removed from list of classpath entries.");
@@ -180,11 +180,11 @@ public class PackrMojo extends AbstractMojo {
                 validClasspathEntries.add(cpEntry);
             }
         }
-        _classpath.clear();
-        _classpath.addAll(validClasspathEntries);
+        classpath.clear();
+        classpath.addAll(validClasspathEntries);
         validClasspathEntries = null;
 
-        if (this._mainClass.contains(".") == false) {
+        if (this.mainClass.contains(".") == false) {
             throw new MojoExecutionException("Main class must contain at least one '.'. Please provide the fully qualified path to the main class.");
         }
 
@@ -197,73 +197,21 @@ public class PackrMojo extends AbstractMojo {
         }
     }
 
-    public void setBundleIdentifier(String _bundleIdentifier) {
-        this._bundleIdentifier = _bundleIdentifier;
-    }
-
-    public void setClasspath(List<String> _classpath) {
-        this._classpath = _classpath;
-    }
-
-    public void setExecutable(String _executable) {
-        this._executable = _executable;
-    }
-
-    public void setIconResource(File _iconResource) {
-        this._iconResource = _iconResource;
-    }
-
-    public void setJdk(String _jdk) {
-        this._jdk = _jdk;
-    }
-
-    public void setJsonConfig(File _jsonConfig) {
-        this._jsonConfig = _jsonConfig;
-    }
-
-    public void setMainClass(String _mainClass) {
-        this._mainClass = _mainClass;
-    }
-
-    public void setMinimizeJre(String _minimizeJre) {
-        this._minimizeJre = _minimizeJre;
-    }
-
-    public void setOutDir(File _outDir) {
-        this._outDir = _outDir;
-    }
-
-    public void setPlatform(PackrConfig.Platform _platform) {
-        this._platform = _platform;
-    }
-
-    public void setResources(List<File> _resources) {
-        this._resources = _resources;
-    }
-
-    public void setVerbose(boolean _verbose) {
-        this._verbose = _verbose;
-    }
-
-    public void setVmArgs(List<String> _vmArgs) {
-        this._vmArgs = _vmArgs;
-    }
-
     @Override
     public String toString() {
-        return "PackrMojo{" + "platform=" + _platform
-                + ", jdk=" + _jdk
-                + ", executable=" + _executable
-                + ", classpath=" + _classpath
-                + ", mainClass=" + _mainClass
-                + ", vmArgs=" + _vmArgs
-                + ", minimizeJre=" + _minimizeJre
-                + ", resources=" + _resources
-                + ", outDir=" + _outDir
-                + ", iconResource=" + _iconResource
-                + ", bundleIdentifier=" + _bundleIdentifier
-                + ", jsonConfig=" + _jsonConfig
-                + ", verbose=" + _verbose + '}';
+        return "PackrMojo{" + "platform=" + platform
+                + ", jdk=" + jdk
+                + ", executable=" + executable
+                + ", classpath=" + classpath
+                + ", mainClass=" + mainClass
+                + ", vmArgs=" + vmArgs
+                + ", minimizeJre=" + minimizeJre
+                + ", resources=" + resources
+                + ", outDir=" + outDir
+                + ", iconResource=" + iconResource
+                + ", bundleIdentifier=" + bundleIdentifier
+                + ", jsonConfig=" + jsonConfig
+                + ", verbose=" + verbose + '}';
     }
 
 }
